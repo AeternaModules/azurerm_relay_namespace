1b8e90744e617454552846ca814f1924cfb4ad6a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,50 +1,45 @@
-output "relay_namespaces" {
-  description = "All relay_namespace resources"
-  value       = azurerm_relay_namespace.relay_namespaces
-  sensitive   = true
-}
 output "relay_namespaces_location" {
-  description = "List of location values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.location]
+  description = "Map of location values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.location }
 }
 output "relay_namespaces_metric_id" {
-  description = "List of metric_id values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.metric_id]
+  description = "Map of metric_id values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.metric_id }
 }
 output "relay_namespaces_name" {
-  description = "List of name values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.name]
+  description = "Map of name values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.name }
 }
 output "relay_namespaces_primary_connection_string" {
-  description = "List of primary_connection_string values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.primary_connection_string]
+  description = "Map of primary_connection_string values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.primary_connection_string }
   sensitive   = true
 }
 output "relay_namespaces_primary_key" {
-  description = "List of primary_key values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.primary_key]
+  description = "Map of primary_key values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.primary_key }
   sensitive   = true
 }
 output "relay_namespaces_resource_group_name" {
-  description = "List of resource_group_name values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.resource_group_name]
+  description = "Map of resource_group_name values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.resource_group_name }
 }
 output "relay_namespaces_secondary_connection_string" {
-  description = "List of secondary_connection_string values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.secondary_connection_string]
+  description = "Map of secondary_connection_string values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.secondary_connection_string }
   sensitive   = true
 }
 output "relay_namespaces_secondary_key" {
-  description = "List of secondary_key values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.secondary_key]
+  description = "Map of secondary_key values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.secondary_key }
   sensitive   = true
 }
 output "relay_namespaces_sku_name" {
-  description = "List of sku_name values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.sku_name]
+  description = "Map of sku_name values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.sku_name }
 }
 output "relay_namespaces_tags" {
-  description = "List of tags values across all relay_namespaces"
-  value       = [for k, v in azurerm_relay_namespace.relay_namespaces : v.tags]
+  description = "Map of tags values across all relay_namespaces, keyed the same as var.relay_namespaces"
+  value       = { for k, v in azurerm_relay_namespace.relay_namespaces : k => v.tags }
 }
 
